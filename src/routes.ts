33ed74c691e7c347915a32ceb506f8d9887e7693
Abdo <abdorabee1134@gmail.com>
2022-03-30 15:42:59 +0200

    added the validateResource

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,6 @@
-import {Express} from 'express';
+import {Express,Request,Response} from 'express';
 function routes(app:Express){
-
-
-
+ app.get('/healthcheck',(req:Request, res:Response)=>res.sendStatus(200));
 }
 
 export default routes ;
