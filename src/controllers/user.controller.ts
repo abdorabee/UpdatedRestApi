e334675c2713ba3ae78f333272ffa155d911d8ba
Abdo <abdorabee1134@gmail.com>
2022-04-05 14:58:30 +0200

    finaly finshed the user registration

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import {Request , Response} from 'express';
+import {omit} from 'lodash';
 import logger from '../utils/logger';
 import {createUser} from '../service/user.service';
 import { CreateUserInput } from "../schema/user.schema";
@@ -6,7 +7,7 @@ import { CreateUserInput } from "../schema/user.schema";
 export async function createUserHandler(req:Request<{},{},CreateUserInput['body']>, res:Response){
 try {
 const user = await createUser(req.body);
-return res.send(user);
+return res.send(omit(user.toJSON()));
 }
 catch(e:any){
  logger.error(e);
---
+++ src/models/session.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import {UserDocument} from './user.model';
+export interface SchemaDocument extends mongoose.Document {
+  user: UserDocument['_id'];
+  valid: boolean;
+  userAgent:String;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const sessionSchema = new mongoose.Schema(
+  {
+    user: { type:mongoose.Schema.Types.ObjectId, ref:'User'},
+    valid: { type: Boolean, default: true },
+    userAgent :{type:String}
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+
+
+
+const SessionModel = mongoose.model("Session", sessionSchema);
+
+export default SessionModel;
