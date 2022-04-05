1b29b4002e0c98a6cd5dddec99bcc1f98162c3b2
Abdo <abdorabee1134@gmail.com>
2022-04-05 15:26:38 +0200

    fixed the json error

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,7 +7,7 @@ import { CreateUserInput } from "../schema/user.schema";
 export async function createUserHandler(req:Request<{},{},CreateUserInput['body']>, res:Response){
 try {
 const user = await createUser(req.body);
-return res.send(omit(user.toJSON()));
+return res.send(user);
 }
 catch(e:any){
  logger.error(e);
